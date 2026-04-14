ceba7350389753615b8aab1f543d58106c741f89
Kauan Maia <maiakauan613@icloud.com>
2026-04-13 21:52:44 -0300

    Adição do metódo save_to_delta no arquivo spark_session.py

--- config/spark_session.py
+++ config/spark_session.py
@@ -14,3 +14,7 @@ class SparkSessionFactory:
             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
         
         return configure_spark_with_delta_pip(builder).getOrCreate()
+    
+    def save_to_delta(self, df, file_path: str, write_mode: str = "overwrite",):
+        writer = df.write.format("delta").mode(write_mode)
+        writer.save(file_path)
